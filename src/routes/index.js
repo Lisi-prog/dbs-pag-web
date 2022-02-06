46f026e8c234af93573003d6378bcc2f8f3c6290
Lisi-prog <80710956+Lisi-prog@users.noreply.github.com>
2022-02-06 20:35:17 -0300

    login y +

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,4 +9,8 @@ router.get("/contact", (req, res) => {
     res.render("contact.html", {tittle: 'Contacto'});
 });
 
+router.get("/login", (req, res) => {
+    res.render("login.html", {tittle: 'Inicio de sesion'});
+});
+
 module.exports = router;
