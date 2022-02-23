256f8026984939d1a5861c39e6cb4cac0b9cd2fc
Lisi-prog <80710956+Lisi-prog@users.noreply.github.com>
2022-02-23 11:11:32 -0300

    login again

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const path = require("path");
 const morgan = require("morgan");
 const passport = require("passport");
 const session = require("express-session");
+const flash = require("connect-flash");
 
 //Conection to server
 mongoose.connect("mongodb+srv://admin:admin@cluster0.eflub.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
@@ -28,8 +29,15 @@ app.use(session({
     resave: false,
     saveUninitialized: false
 }));
+app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use((req, res, next) => {
+    app.locals.signupMessage = req.flash("signupMessage");
+    next();
+});
+
 
 //Routes
 app.use(require("./routes/index"));
--- src/passport/local-auth.js
+++ src/passport/local-auth.js
@@ -17,9 +17,15 @@ passport.use("local-signup", new LocalStrategy({
     passwordField: "password",
     passReqToCallback: true
 }, async (req, email, password, done) => {
-    const user = new User();;
-    user.email = email;
-    user.password = password;
-    await user.save();
-    done(null, user);
+
+    const user = User.findOne({email: email});
+    if(user){
+        return done(null, false, req.flash("signupMessage","Ya tiene una cuenta"));
+    }else{
+        const newUser = new User();;
+        newUser.email = email;
+        newUser.password = newUser.encryptPassword(password);
+        await newUser.save();
+        done(null, newUser);
+    }
 }));
