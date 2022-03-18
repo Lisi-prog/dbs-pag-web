53bcd568d677b1d5642f51353a5434a44197088d
Lisi-prog <80710956+Lisi-prog@users.noreply.github.com>
2022-03-17 21:22:17 -0300

    17/3

--- src/public/js/imagePre.js
+++ src/public/js/imagePre.js
@@ -1,15 +1,34 @@
 function createPreview(file){
     var imgCodified = URL.createObjectURL(file);
-    console.log(imgCodified);
-    const node = document.getElementById("botonn");
-    const img = $('<div class="col-xl-2 col-lg-2 col-md-3 col-sm-4 col-xs-12"><div class="image-container"> <figure> <img src="' + imgCodified + '" alt="Foto del usuario"> <figcaption> <i class="icon-cross"></i> </figcaption> </figure> </div></div>');
-    img.insertBefore(node, null);
+    const newDiv = document.createElement("div");
+    document.create
+    const node = document.getElementById("boy");
+    newDiv.classList.add("col-sm");
+    newDiv.setAttribute("id","elementos");
+    
+    // const node = document.getElementById("botonn");
+    // const img = '<div class="col-xl-2 col-lg-2 col-md-3 col-sm-4 col-xs-12"><div class="image-container"> <figure> <img style="height:200px" src="' + imgCodified + '" alt="Foto del usuario"> <figcaption> <i class="icon-cross"></i> </figcaption> </figure> </div></div>';
+    const img = '<img style="height:200px" src="' + imgCodified + '" alt="Foto del usuario">';
+
+    newDiv.innerHTML = img;
+    node.appendChild(newDiv);
+    // img.insertBefore(node, null);
   }
 
 const myInput = document.getElementById('addPhoto01');
 
 myInput.addEventListener("change", (evt) => {
     var files = evt.target.files; // FileList object
+
+    const node = document.getElementById("boy");
+    const child = document.getElementById("fondo-photo");
+
+    if (child){
+        node.removeChild(child);
+    }else{
+        node.innerHTML = "";     
+    }
+
     var element;
     for(var i = 0; i < files.length; i++){
         element = files[i];
